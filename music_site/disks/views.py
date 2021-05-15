bce3dafea5098a93c0da5ed0bf75dc1b1543b15e
C-Ky <claireky98@gmail.com>
2021-05-15 17:37:27 +0200

    displaying all albums

--- music_site/disks/views.py
+++ music_site/disks/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from disks.models import Album, Artist, Track
 
-# Create your views here.
+
+def list_albums(request):
+    """ Displays a list of all all albums """
+    albums = Album.objects.all()
+    return render(request, 'disks/albums_list.html', locals())
