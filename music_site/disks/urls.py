1687e33c10332dba02c015696b28752e699bbdda
C-Ky <claireky98@gmail.com>
2021-05-15 17:59:32 +0200

    adding link to album info page

--- music_site/disks/urls.py
+++ music_site/disks/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.list_albums),
+    path('albums/<int:id>', views.album_info, name='album_info'),
 ]
--- music_site/disks/views.py
+++ music_site/disks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from disks.models import Album, Artist, Track
 
 
@@ -6,3 +6,10 @@ def list_albums(request):
     """ Displays a list of all all albums """
     albums = Album.objects.all()
     return render(request, 'disks/albums_list.html', locals())
+
+
+def album_info(request, id):
+    """ Displays all tracks of an album """
+    album = get_object_or_404(Album, id=id)
+    tracks = Track.objects.filter(album=album)
+    return render(request, 'disks/album_info.html', locals())
