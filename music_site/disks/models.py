402bea1d4218b2f7a86a5f08ad861b49e5b6d291
C-Ky <claireky98@gmail.com>
2021-05-15 16:51:46 +0200

    adding models

--- music_site/disks/models.py
+++ music_site/disks/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+
+class Album(models.Model):
+    title = models.CharField(max_length=200)
+    artist = models.ForeignKey('Artist', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{0} by {1}".format(self.title, self.artist)
+
+
+class Track(models.Model):
+    name = models.CharField(max_length=200)
+    composer = models.CharField(max_length=200)
+    milliseconds = models.IntegerField()
+    bytes = models.IntegerField
+    unitPrice = models.DecimalField(max_digits=3, decimal_places=2)
+    album = models.ForeignKey('Album', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
